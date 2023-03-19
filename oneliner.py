3b594ec133a52556f18b2f3f32c95c37421f6cd1
justjaym <110678511+justjaym@users.noreply.github.com>
2023-03-19 23:28:26 +0800

    update

---
+++ oneliner.py
@@ -0,0 +1,24 @@
+# SWAP
+a = 1
+b = 1
+a, b = b, a
+
+#Even/Odd numbers
+print([x for x in range(11) if x % 2 == 1])
+
+#reverse
+lis = [1, 2, 3]
+lis.reverse()
+ 
+print(lis)
+
+
+#factorial
+import math
+n = 5
+print(math.factorial(n))
+
+#shortcut instead of multiple ifs
+m = 2
+if m in [1, 2, 3]:
+    pass
